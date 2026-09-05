42dbe58f5331c35815d4297cca2ec14d68f4caf6
SUMIT YADAV <sumitsagar2405@gmail.com>
2026-09-05 06:57:44 +0000

    data conversion done

---
+++ javascriptwithchai/03conversionoperation.js
@@ -0,0 +1,55 @@
+// // we can forcefully modify the data type 
+
+// let score="33";
+
+// console.log(typeof(score));
+
+
+// let scoreInNumber=Number(score)
+
+// console.log(typeof scoreInNumber);
+
+// console.log(scoreInNumber);
+
+// score="33abc";
+
+// console.log(typeof(score));
+
+
+// scoreInNumber=Number(score)
+
+// console.log(typeof scoreInNumber);
+
+// console.log(scoreInNumber);
+
+
+// score=null
+
+// console.log(typeof(score));
+
+
+// scoreInNumber=Number(score)
+
+// console.log(typeof scoreInNumber);
+
+// console.log(scoreInNumber);
+
+
+// let isLoggedIn='123sumit';
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+
+
+// console.log(booleanIsLoggedIn);
+
+// 1  true
+// 0 false
+// ''  false
+
+//  'hitesh'   true
+
+ let someNumber=33;
+
+ let stringNumber=String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 

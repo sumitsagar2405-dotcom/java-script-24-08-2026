b22796fa214e66adf4871b743833387db955dfcf
SUMIT YADAV <sumitsagar2405@gmail.com>
2026-09-07 11:14:32 +0000

    scope completed

---
+++ javascriptwithchai/JAVASCRIPTWITHCHAIadvance/02scopes.js
@@ -0,0 +1,34 @@
+// let a=10;  // here a, b, c, is a global scope variable
+// const b=20;
+// var c=30;
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+{}// called scope
+
+// if (true){
+//     let a=40;
+//     const b=50;// here a, b, c are block scope variable
+//     var c=60;
+    
+// }
+// //console.log(a);
+// //console.log(b);
+// console.log(c); // we can acces var out of the scope because of this reason we not prefer to use var
+
+// if (true){
+//     let a=40;
+//     const b=50;
+//     c=60;  // also create a problem
+    
+// }
+// //console.log(a);
+// //console.log(b);
+// console.log(c); // we can acces var out of the scope because of this reason we not prefer to use var
+
+
+
